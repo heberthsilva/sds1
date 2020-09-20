27afd15fe19bf09c34c617c2dbc137d6283500c6
Heberth Silva <heberth.silva@live.com>
2020-09-20 04:45:17 -0300

    Adicionando Header

---
+++ front-web/src/components/Header/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {ReactComponent as Logo} from '../../assets/logo.svg';
+import './styles.css';
+
+
+
+
+
+const Header =() => (
+
+    <head className="main-header">
+        <Logo/>
+<div className="logo-text">
+    <span className="logo-text-1">Big Game</span>
+    <span className="logo-text-2">Survey</span>
+</div>
+
+
+    </head>
+
+);
+
+
+
+
+export default Header
